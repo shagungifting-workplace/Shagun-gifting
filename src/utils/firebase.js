4c3e7aaeedaf2ee92b43cb84973a3a5651b11e13
Nabin Agrawal <nabinagrawal64@gmail.com>
2025-07-15 10:00:15 +0530

    add env file

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,18 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB0xA8sb1-LQPoiOaGnLMzZwp3jJG4OpMI",
-    authDomain: "shagun-gifting.firebaseapp.com",
-    projectId: "shagun-gifting",
-    storageBucket: "shagun-gifting.firebasestorage.app",
-    messagingSenderId: "125393577354",
-    appId: "1:125393577354:web:3995616ef514066cca5473",
-    measurementId: "G-YYGY9LZ0DN",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -20,4 +20,4 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
 export const db = getFirestore(app);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
