604e1e865b8adec6cc33b682b8d2f6257a7e9521
Nabin Agrawal <nabinagrawal64@gmail.com>
2025-07-22 07:12:43 +0530

    add loader on refresh

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Toaster } from 'react-hot-toast';
+import { StrictMode, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Toaster } from "react-hot-toast";
+import GlobalLoader from "./components/GlobalLoader";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-    <Toaster />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <Suspense fallback={<GlobalLoader />}>
+            <App />
+            <Toaster />
+        </Suspense>
+    </StrictMode>
+);
